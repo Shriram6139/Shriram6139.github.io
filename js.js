69fc1035e1a9ba54556ac87bef26c9aa144a3a47
Shriram6139 <70508131+Shriram6139@users.noreply.github.com>
2020-09-10 01:57:21 +0530

    Enlarge image added

---
+++ js.js
@@ -0,0 +1,8 @@
+function imgModal(x){
+    document.getElementById("myImgModal").style.display="block";
+    document.getElementById("img01").src= "./otherworks/" + x + ".jpg";
+}
+
+function funcImgModalClose(){
+    document.getElementById("myImgModal").style.display="none";
+}
