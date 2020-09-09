eb460b0fc1dd4f276ab7c8c0a1eb26370900929b
Shriram6139 <70508131+Shriram6139@users.noreply.github.com>
2020-09-10 02:01:42 +0530

    minor changes

--- js.js
+++ js.js
@@ -1,6 +1,11 @@
 function imgModal(x){
     document.getElementById("myImgModal").style.display="block";
+    if(x != 12 && x != 10){
     document.getElementById("img01").src= "./otherworks/" + x + ".jpg";
+    }
+    else{
+        document.getElementById("img01").src= "./otherworks/" + x + ".JPG";
+    }
 }
 
 function funcImgModalClose(){
